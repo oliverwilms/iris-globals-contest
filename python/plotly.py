e67a8aa1a141525d9f23bca926592eee61e109fe
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-09-01 13:25:01 -0500

    query = "SELECT Top 10 Category, Credit, Debit, TrnCount FROM dc_iris.trncount where TrnYear=2025 and TrnMonth=8 Order By Debit DESC"

--- python/plotly.py
+++ python/plotly.py
@@ -1,12 +1,10 @@
 import iris
 import plotly.express as px
 
-query = "SELECT ..."
-df = iris.sql.exec(query).dataframe().sort_values(by=['column'], ascending=False)
+query = "SELECT Top 10 Category, Credit, Debit, TrnCount FROM dc_iris.trncount where TrnYear=2025 and TrnMonth=8 Order By Debit DESC"
+df = iris.sql.exec(query).dataframe().sort_values(by=['Debit'], ascending=False)
 
-fig = px.bar(df.head(10),x="location",y="column",barmode="group",text_auto='.3s')
-
+fig = px.bar(df.head(10),x="Category",y="Debit",barmode="group",text_auto='.3s')
 fig.update_traces(textfont_size=12,textangle=0,textposition="outside",cliponaxis=False)
-
 fig.update_layout(height=330)
 fig.show()
