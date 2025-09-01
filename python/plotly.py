75cb6ab11bf7623a9f172c93e8290b0587f9cafa
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-09-01 14:40:59 -0500

    Generate the HTML div

--- python/plotly.py
+++ python/plotly.py
@@ -1,5 +1,8 @@
+# This is a basic Python program
 import iris
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import plot
 
 query = "SELECT Top 10 Category, Credit, Debit, TrnCount FROM dc_iris.trncount where TrnYear=2025 and TrnMonth=8 Order By Debit DESC"
 df = iris.sql.exec(query).dataframe().sort_values(by=['Debit'], ascending=False)
@@ -7,4 +10,15 @@ df = iris.sql.exec(query).dataframe().sort_values(by=['Debit'], ascending=False)
 fig = px.bar(df.head(10),x="Category",y="Debit",barmode="group",text_auto='.3s')
 fig.update_traces(textfont_size=12,textangle=0,textposition="outside",cliponaxis=False)
 fig.update_layout(height=330)
-fig.show()
+# fig.show()
+div = plotly.offline.plot(fig, include_plotlyjs=False, output_type='div')
+# Print or save the div
+with open("plot.html", "w") as file:
+    file.write(f"<html><body>{div}</body></html>")
+
+
+# Create a simple plot
+# fig = go.Figure(data=[go.Bar(x=['A', 'B', 'C'], y=[10, 20, 30])])
+
+# Generate the HTML div
+# div = plot(fig, output_type='div')
