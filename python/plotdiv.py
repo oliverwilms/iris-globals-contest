bd0c9fbf9d5dfdcc25f0c839c1025ec03d729681
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-09-06 17:49:45 -0500

    https://plotly.com/python/setting-graph-size/

--- python/plotdiv.py
+++ python/plotdiv.py
@@ -24,6 +24,32 @@ from plotly.offline import plot
 #   'Total': [236, 241, 259]
 # })
 
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    x=["Apples", "Oranges", "Watermelon", "Pears"],
+    y=[3, 2, 1, 4]
+))
+
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    yaxis=dict(
+        title=dict(
+            text="Y-axis Title",
+            font=dict(
+                size=30
+            )
+        ),
+        ticktext=["Very long label", "long label", "3", "label"],
+        tickvals=[1, 2, 3, 4],
+        tickmode="array",
+    )
+)
+
+fig.update_yaxes(automargin=True)
+
 # pyo.plot(fig)
 
 # Generate the HTML div
@@ -32,17 +58,17 @@ def gendiv(trnyear, trnmonth, filename="titanic.csv"):
     print(query)
     df = iris.sql.exec(query).dataframe().sort_values(by=['debit'],ascending=False)
     ##fig = px.bar(df.head(10),x="category",y="debit")
-    fig = go.Figure(
-       data=[
-           go.Bar(
-               x=df['category'],
-               y=df['debit']
-           )
-       ],
-       layout=go.Layout(
-           title='Total Monthly Cost'
-       )
-    )
+    #fig = go.Figure(
+    #   data=[
+    #       go.Bar(
+    #           x=df['category'],
+    #           y=df['debit']
+    #       )
+    #   ],
+    #   layout=go.Layout(
+    #       title='Total Monthly Cost'
+    #   )
+    #)
     # fig.update_traces(textfont_size=12,textangle=0,textposition="outside",cliponaxis=False)
     ##fig.update_traces(hovertemplate='%{y:.2f}', texttemplate='%{y:.2f}')
     # fig.update_layout(height=330)
